94d9fb0ecf7746ba44816a621ec629d51189da04
Tom Pittson <tom.pittson@capgemini.com>
2019-05-06 18:17:04 +0100

    Updated for data demo.

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "NoSQL")
+option_b = os.getenv('OPTION_B', "RDBMS")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
